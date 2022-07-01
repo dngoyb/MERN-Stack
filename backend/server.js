4d71a2c50b92c15eabf7bc63619ce3018df9db5b
dngoyb <dngoyb@gmail.com>
2022-07-01 21:40:10 +0200

    Initial commit, creating express server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+//Express app
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+	res.json({ msg: 'welcome to the app' });
+});
+
+// Listen for request
+app.listen(4000, () => console.log('listening on port 4000!!!'));
